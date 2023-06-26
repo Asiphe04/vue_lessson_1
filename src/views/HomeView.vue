227f20ee364bd95b262ec27ad809bd9867460011
Asiphe04 <asiphendimlana40@gmail.com>
2023-06-26 14:26:20 +0200

    second_commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <p v-if="'path ===/'">You are on Home Page</p>
+  <p v-else>You're on {{ path }}</p> -->
+
+  <p><span :title="message">Hover & see the dynamic title</span></p>
+  <p :class="{red:isRed}" @click="toggleRed">This should be red....But click me to toggle</p>
+  <p :style="{color}" @click="toggleColor">This will change color when clicking</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld
+//   }
+// }
+export default
+ {data()
+   {return{message: 'Hello World', isRed: true, color: 'green'}},
+   methods: {
+  toggleRed(){this.isRed = !this.isRed},
+  toggleColor(){this.color = this.color ==='green' ? 'blue' : 'green'}
 }
+}
+// data:{path: Location.pathname}
+
+
 </script>
+<style>
+.red{color: red;}</style>
